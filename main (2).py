6c5815984cebdf69e7f334d142205de54df5b804
chandana-2005-hub <nandyalachandana85@gmail.com>
2025-12-24 10:55:13 +0530

    Add files via upload

---
+++ main (2).py
@@ -0,0 +1,15 @@
+''''triangle pattern'''
+
+x=int(input())
+for i in range(x):
+    for j in range(i+1):
+        print("*",end=" ")
+    print(i)
+    
+ '''square pattern'''   
+    
+x=int(input())
+for i in range(x):
+    for j in range(x):
+        print("*",end=" ")
+    print(i)
