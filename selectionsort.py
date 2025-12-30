dd994a7b075d3878f6602f23bc8969d65dff4c6c
chandana-2005-hub <nandyalachandana85@gmail.com>
2025-12-30 15:58:14 +0530

    Add files via upload

---
+++ selectionsort.py
@@ -0,0 +1,11 @@
+def selection_sort(arr):
+    n=len(arr)
+    for i in range(n):
+        lower=i
+        for j in range(i+1,n):
+            if arr[j]<arr[lower]:
+                lower=j
+                arr[j],arr[lower]=arr[lower],arr[i]
+arr=[4,48,26]
+selection_sort(arr)
+print("sorted array:",arr)
